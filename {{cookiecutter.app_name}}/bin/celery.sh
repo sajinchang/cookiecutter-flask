6eaad152ee6c6c54331f888dd7b110ba6372ee36
SamSa <sajinde@163.com>
2023-07-24 21:36:47 +0800

    [fix] celery start pid file

--- {{cookiecutter.app_name}}/bin/celery.sh
+++ {{cookiecutter.app_name}}/bin/celery.sh
@@ -13,23 +13,23 @@ LOG_DIR="${HOME}/logs/${PROJECT_NAME}"
 case "${name}" in
 worker|-w|-W)
     # celery  multi start -A "${PROJECT_NAME}.tasks.celery_app:app" worker -fair \
-    test -e "${LOG_DIR}/celert-worker.pid" && rm -f "${LOG_DIR}/celert-worker.pid"
+    test -e "${LOG_DIR}/celery-worker.pid" && rm -f "${LOG_DIR}/celery-worker.pid"
     celery \
         -A "${PROJECT_NAME}.tasks.celery_app:app" worker \
         -fair \
         -E \
         -l INFO  \
         --logfile="${LOG_DIR}/worker.log" \
-        --pidfile="${LOG_DIR}/celert-worker.pid"
+        --pidfile="${LOG_DIR}/celery-worker.pid"
         # -D # --detach      Start worker as a background process.
         # -n, --hostname HOSTNAME         Set custom hostname (e.g., 'w1@%%h').
     ;;
 beat|-b|-B)
-    test -e "${LOG_DIR}/celert-beat.pid" && rm -f "${LOG_DIR}/celert-beat.pid"
+    test -e "${LOG_DIR}/celery-beat.pid" && rm -f "${LOG_DIR}/celery-beat.pid"
     celery \
         -A "${PROJECT_NAME}.tasks.celery_app:app" beat \
         -l INFO \
-        --pidfile "${LOG_DIR}/celert-beat.pid" \
+        --pidfile "${LOG_DIR}/celery-beat.pid" \
         -s "${LOG_DIR}/celerybeat-schedule" \
         --logfile="${LOG_DIR}/beat.log"
         # --detach    Detach and run in the background as a daemon.
